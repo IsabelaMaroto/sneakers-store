f3fb695284a0e52ae430680fe24b190136299de3
IsabelaMaroto <belamaroto@gmail.com>
2023-04-13 17:55:26 -0300

    finished mobile template

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import styled from '@mui/styled-engine-sc'
 import {Header} from './components/header/header';
 import { Main } from './components/main/mainContainer';
 const Container = styled('div')`
-  
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 `
 
 function App() {
---
+++ src/components/main/listTexts.js
@@ -0,0 +1,10 @@
+export const Texts = [
+    {
+        brand: 'Sneaker Company',
+        title: 'Fall Limited Edition Sneakers',
+        description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
+        priceTotal: '$125.00',
+        discount: '50%',
+        price: '$250.00',
+    }
+]
